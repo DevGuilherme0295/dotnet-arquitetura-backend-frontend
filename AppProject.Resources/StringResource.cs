b2eea03bfeb154ce47fa9934409edbb65513c881
DevGuilherme0295 <guilhermemacfaria@gmail.com>
2026-04-16 08:09:27 -0300

    Creating 'Resources'

---
+++ AppProject.Resources/StringResource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Company.ClassLibrary1;
+
+public static class StringResource
+{
+    public static string GetStringByKey(string key, params object?[] args)
+    {
+        var message = ResourceAttributes.ResourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? string.Empty;
+
+        return string.Format(message, args);
+    }
+}
