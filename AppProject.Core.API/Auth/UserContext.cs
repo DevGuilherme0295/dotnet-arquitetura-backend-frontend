919daf545bcb5121ce3fa928f71d55f5ceefe62a
DevGuilherme0295 <guilhermemacfaria@gmail.com>
2026-08-04 09:25:04 -0300

    Implementa GetSystemAdminUserAsync no UserContext

--- AppProject.Core.API/Auth/UserContext.cs
+++ AppProject.Core.API/Auth/UserContext.cs
@@ -1,13 +1,31 @@
 using System;
 using AppProject.Core.Contracts;
+using AppProject.Core.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppProject.Core.API.Auth;
 
-public class UserContext : IUserContext
+public class UserContext(
+    ApplicationDbContext applicationDbContext) 
+    : IUserContext
 {
-    public Task<UserInfo> GetSystemAdminUserAsync(CancellationToken cancellationToken = default)
+
+    public async Task<UserInfo> GetSystemAdminUserAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = await applicationDbContext.Users.FirstOrDefaultAsync(u => u.IsSystemAdmin, cancellationToken);
+
+        if (user is null)
+        {
+            throw new InvalidOperationException("System admin user not found.");
+        }
+
+        return new UserInfo
+        {
+            UserId = user.Id,
+            UserName = user.Name,
+            Email = user.Email,
+            IsSystemAdmin = true
+        };
     }
 
     public Task<UserInfo> GetCurrentUserAsync(CancellationToken cancellationToken = default)
